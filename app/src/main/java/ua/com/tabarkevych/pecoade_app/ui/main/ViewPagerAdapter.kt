d685e6557c7bed1f85381c3cc31a4af084b5ee9a
mixtab <m.tabarkevych@gmail.com>
2021-12-05 13:27:30 +0200

    Add adapter for ViewPager

---
+++ app/src/main/java/ua/com/tabarkevych/pecoade_app/ui/main/ViewPagerAdapter.kt
@@ -0,0 +1,81 @@
+package ua.com.tabarkevych.pecoade_app.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ua.com.tabarkevych.pecoade_app.data.s.entity.Page
+import ua.com.tabarkevych.pecoade_app.databinding.ItemViewPagerBinding
+
+class ViewPagerAdapter(
+    private var listener: EventListener
+) : ListAdapter<Page, RecyclerView.ViewHolder>(PageDiffCallback)
+{
+     var pagesList = emptyList<Page>()
+
+    interface EventListener {
+        fun onNotificationClicked(position: Int)
+        fun onButtonIncreaseClicked(position: Int)
+        fun onButtonDecreaseClicked(lastItem: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerItemViewHolder {
+        return ViewPagerItemViewHolder(
+            ItemViewPagerBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    fun setData(pages: List<Page>) {
+        this.pagesList = pages
+    }
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val vh =  (holder as ViewPagerItemViewHolder)
+        with(vh.binding) {
+            textNotification.setOnClickListener {
+                listener.onNotificationClicked(position + 1)
+            }
+
+            imageDecrease.visibility = if (position == 0) View.GONE else View.VISIBLE
+            imageRectangle.text = (position + 1).toString()
+
+            imageIncrease.setOnClickListener { listener.onButtonIncreaseClicked(position + 2) }
+            imageDecrease.setOnClickListener { listener.onButtonDecreaseClicked(itemCount) }
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return pagesList.size
+    }
+
+    class ViewPagerItemViewHolder(var binding: ItemViewPagerBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+
+}
+
+private object PageDiffCallback : DiffUtil.ItemCallback<Page>() {
+    override fun areItemsTheSame(
+        oldItem: Page,
+        newItem: Page
+    ): Boolean {
+        val oldItemId = oldItem.id
+        val newItemId = newItem.id
+        return oldItemId == newItemId
+    }
+
+    override fun areContentsTheSame(
+        oldItem: Page,
+        newItem: Page
+    ): Boolean {
+        return oldItem == newItem
+    }
+}
